73a740ce743726c68739648b1d4a686f87286cc3
Emil <kurbanov_emil@icloud.com>
2021-12-06 12:07:46 +0300

    Lesson2

--- swiftUICourse/swiftUICourseApp.swift
+++ swiftUICourse/swiftUICourseApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct swiftUICourseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           // ContentView()
+            Chat()
         }
     }
 }
